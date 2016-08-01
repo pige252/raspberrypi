9eeaccced1d13cace6441710b8e7164eec8b8933
pige252@gmail.com <pi@localhost.localdomain>
2016-08-01 10:02:33 +0900

    change testmysql.py

--- testmysql.py
+++ testmysql.py
@@ -1,20 +1,42 @@
 #!/usr/bin/python
 
 import MySQLdb
+import sys
 
-# Open database connection, user,      id,        db, table
-db = MySQLdb.connect("localhost","TESTID","Test!234","test" )
+try:
+    db = MySQLdb.connect("localhost",TESTID","Test!234","test")
+
+    cursor = db.cursor()
+    cursor.execute("SELECT VERSION()")
+
+    data = cursor.fetchone()
+
+    print ("Database version : %s " %data)
+
+excpet MySQLdb, e:
+
+    print "Error %d:%s"%(e.args[0],e.args[1])
+    sys.exit(1)
+
+finally:
+    if con:
+        db.close()
+
+
+# old version
+# Open database connection, user,      id,      pass,   db
+#db = MySQLdb.connect("localhost","TESTID","Test!234","test" )
 
 # prepare a cursor object using cursor() method
-cursor = db.cursor()
+#cursor = db.cursor()
 
 # execute SQL query using execute() method.
-cursor.execute("SELECT VERSION()")
+#cursor.execute("SELECT VERSION()")
 
 # Fetch a single row using fetchone() method.
-data = cursor.fetchone()
+#data = cursor.fetchone()
 
-print "Database version : %s " % data
+#print "Database version : %s " % data
 
 # disconnect from server
-db.close()
+#db.close()
